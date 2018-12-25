167c761e5674ab2ad87cb4ffbf85f09bc4d08289
Kareem Mohamed <kareem3d.a@gmail.com>
2018-12-25 14:17:41 +0100

    Part4: Render correct chess pieces on the board

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import ChessBoard from './components/ChessBoard';
-import ChessPiece from './components/ChessPiece';
-import { SizeMe } from 'react-sizeme';
+import ChessGame from './components/ChessGame';
 
 class App extends Component {
-
-  renderAtPosition = (wrapperWidth, { x, y }) => (
-    <ChessPiece
-      pieceName={'bishop'}
-      color={'white'}
-      boxWidth={wrapperWidth / 8}
-    />
-  )
-
   render() {
     return (
-      <SizeMe>
-        {({ size }) => (
-          <ChessBoard
-            primaryColor={'#EEEED5'}
-            secondaryColor={'#7D955D'}
-            wrapperWidth={size.width || -1}
-            renderAtPosition={(...args) =>
-              this.renderAtPosition(size.width, ...args)}
-          />
-        )}
-      </SizeMe>
+      <ChessGame />
     );
   }
 }
---
+++ src/components/ChessGame/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { SizeMe } from 'react-sizeme';
+import range from 'lodash/range';
+import ChessBoard from '../ChessBoard';
+import ChessPiece from '../ChessPiece';
+
+const pieces = [
+  [
+    { name: 'rook', color: 'black' },
+    { name: 'knight', color: 'black' },
+    { name: 'bishop', color: 'black' },
+    { name: 'queen', color: 'black' },
+    { name: 'king', color: 'black' },
+    { name: 'bishop', color: 'black' },
+    { name: 'knight', color: 'black' },
+    { name: 'rook', color: 'black' },
+  ],
+  range(8).map(_ => ({ name: 'pawn', color: 'black' })),
+  [],
+  [],
+  [],
+  [],
+  range(8).map(_ => ({ name: 'pawn', color: 'white' })),
+  [
+    { name: 'rook', color: 'white' },
+    { name: 'knight', color: 'white' },
+    { name: 'bishop', color: 'white' },
+    { name: 'queen', color: 'white' },
+    { name: 'king', color: 'white' },
+    { name: 'bishop', color: 'white' },
+    { name: 'knight', color: 'white' },
+    { name: 'rook', color: 'white' },
+  ],
+];
+
+class ChessGame extends React.Component {
+
+  renderAtPosition = (wrapperWidth, { x, y }) => {
+    const piece = pieces[x][y];
+
+    if (!piece) {
+      return null;
+    }
+
+    const { name, color } = piece;
+
+    return (
+      <ChessPiece
+        pieceName={name}
+        color={color}
+        boxWidth={wrapperWidth / 8}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <SizeMe>
+        {({ size }) => (
+          <ChessBoard
+            primaryColor={'#EEEED5'}
+            secondaryColor={'#7D955D'}
+            wrapperWidth={size.width || -1}
+            renderAtPosition={(...args) =>
+              this.renderAtPosition(size.width, ...args)}
+          />
+        )}
+      </SizeMe>
+    );
+  }
+}
+
+export default ChessGame;
